f335a80c63ac29523224c5fbf7dc8fd5482f2f6d
jayantve <jayantv892@gmail.com>
2025-03-31 22:35:03 +0530

    dhg

--- skfms/Section/HKSection.js
+++ skfms/Section/HKSection.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
+import image6 from '@/Image/CurtainCleaning.jpg'
+import image5 from '@/Image/SofaCleaning.jpg'
+import image4 from '@/Image/CarpetCleaning.jpg'
+import image3 from '@/Image/WindowCleaning.jpg'
+import image2 from '@/Image/DeepCleaning.jpg'
+import image1 from '@/Image/ASecImg.jpg'
 
 const HKSection = () => {
     return (
@@ -8,42 +15,42 @@ const HKSection = () => {
                     <div className="flex flex-wrap -mx-4 -mb-10 text-center">
                         <div className="sm:w-1/2 mb-10 px-4">
                             <div className="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1201x501" />
+                            <Image alt="content" className="object-cover object-center h-full w-full" src={image1} />
                             </div>
                             <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">General Cleaning</h2>
                             <p className="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
                         </div>
                         <div className="sm:w-1/2 mb-10 px-4">
                             <div className="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1202x502" />
+                            <Image alt="content" className="object-cover object-center h-full w-full" src={image2} />
                             </div>
                             <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Deep Cleaning</h2>
                             <p className="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
                         </div>
                         <div className="sm:w-1/2 mb-10 px-4">
                             <div className="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1201x501" />
+                            <Image alt="content" className="object-cover object-center h-full w-full" src={image3} />
                             </div>
                             <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Window Glass Cleaning</h2>
                             <p className="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
                         </div>
                         <div className="sm:w-1/2 mb-10 px-4">
                             <div className="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1202x502" />
+                                <Image alt="content" className="object-cover object-center h-full w-full" src={image4} />
                             </div>
                             <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Carpet Cleaning</h2>
                             <p className="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
                         </div>
                         <div className="sm:w-1/2 mb-10 px-4">
                             <div className="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1201x501" />
+                                <Image alt="content" className="object-cover object-center h-full w-full" src={image5} />
                             </div>
                             <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Sofa Shampoo</h2>
                             <p className="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
                         </div>
                         <div className="sm:w-1/2 mb-10 px-4">
                             <div className="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1202x502" />
+                                <Image alt="content" className="object-cover object-center h-full w-full" src={image6} />
                             </div>
                             <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">Curtains Cleaning</h2>
                             <p className="leading-relaxed text-base">Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.</p>
